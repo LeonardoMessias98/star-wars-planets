8b2d1fd10265b93e6f24c71bb8e95ff1bab64090
LeonardoMessias98 <messiasleonardo98@gmail.com>
2023-12-14 02:45:45 -0300

    fix(Home): fixing page height

--- src/modules/Home/styles.ts
+++ src/modules/Home/styles.ts
@@ -4,7 +4,7 @@ export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  min-height: calc(100vh - 185px);
+  min-height: calc(100vh - 74px);
   padding: 74px 20px;
 
   background: linear-gradient(0deg, #000000d0, transparent);
